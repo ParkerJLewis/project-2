8e14e97120cf434e609707000803f0c247e38261
Parker Lewis <parkerl@uoregon.edu>
2020-04-24 20:56:53 -0700

    Requirement 1 and Lab 3

--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,15 +1,15 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function(){
 
-    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
+    let prefixURL = "https://api.flickr.com/services/feeds/photos_public.gne?tags=";
     let suffixURL = "&format=json&jsoncallback=?";
     //get value entered by user from textbox
-    let flickrTag = $("input").???();
+    let flickrTag = $("input").val();
     let requestURL = prefixURL + flickrTag + suffixURL;
 
     //clear old photos
-    $(".photos").???("");
+    $(".photos").html("");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
@@ -36,5 +36,12 @@ let main = function(){
   });
 };
 
+//register controller
+window.addEventListener("load", () => {
+  //select button
+  let button = document.querySelector("button");
 
-$(document).ready(main);
+  //register button
+  button.addEventListener("click", controller);
+});
+//$(document).ready(controller);
